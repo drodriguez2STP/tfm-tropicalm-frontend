102cfb606d2cb0ad1629ecb2a6f3a1aa820db00a
DannyRodriguezFSWD <drodriguez2@student.threepoints.com>
2023-03-01 03:13:32 -0600

    Creando el AuthContext y useAuth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
+import React, { useMemo } from "react";
 import { ToastContainer } from "react-toastify"
-
+import AuthContext from "../context/AuthContext";
 import "../scss/global.scss"
 import 'semantic-ui-css/semantic.min.css'
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
+  const authData = useMemo(
+    () => ({
+      auth: { name: "Dante", email: "dante@gmial.com"},
+      login: () => null,
+      logout: () => null,
+      setReloadUser: () => null,
+    }),
+    []
+  );
   return (
-    <>
+    <AuthContext.Provider value={authData}>
       <Component {...pageProps} />
       <ToastContainer 
         position="top-right"
@@ -19,6 +29,6 @@ export default function App({ Component, pageProps }) {
         draggable
         pauseOnHover      
       />
-    </>
+    </AuthContext.Provider>
     )
 }
